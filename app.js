3c846a402c8a2508004a0d8ea862e2b85713507b
Ian Klug <ianklug123@gmail.com>
2023-01-08 15:05:43 -0800

    add dom manipulation for addbooks sorta

--- app.js
+++ app.js
@@ -4,12 +4,11 @@ let myLibrary = [];
 function Book(title, author, pages, haveRead) {
     this.title = title;
     this.author = author;
-    this.pages = pages;
     this.haveRead = haveRead;
     this.info = function() {
         let readingStatus = '';
         haveRead ? readingStatus+='has been read' : readingStatus+='have not read yet';
-        return `${title} by ${author}, ${pages} pages, ${readingStatus}.`;
+        return `${title} by ${author}, ${readingStatus}.`;
     }
 
 }
@@ -39,3 +38,45 @@ console.log( `${bookObject.title} has been added to the Library.`);
 }
 addBookToLibrary(book1);
 console.log(myLibrary);
+
+// html related manipulation
+
+const bookList = document.querySelector('div.book-display');
+
+const addNewBook = (title, author, haveRead) => {
+    const bookList = document.querySelector('div.book-display');
+    const newBook = document.createElement('div');
+newBook.classList.add('listed-book');
+newBook.setAttribute('data-index', `${myLibrary.length}`);
+const bookTitle = document.createElement('div');
+bookTitle.classList.add('book-title');
+bookTitle.textContent = title;
+const bookAuthor = document.createElement('div');
+bookAuthor.textContent = author;
+const buttonDiv = document.createElement('div');
+const readButton = document.createElement('button');
+readButton.classList.add('listed');
+haveRead ? readButton.classList.add('read-border') : readButton.classList.add('unread-border');
+
+haveRead ? readButton.textContent = 'Read' : readButton.textContent = 'Not Read';
+buttonDiv.appendChild(readButton);
+newBook.appendChild(bookTitle);
+newBook.appendChild(bookAuthor);
+newBook.appendChild(buttonDiv);
+bookList.appendChild(newBook);
+}
+addNewBook('The Lord of the Rings', 'J.R.R Tolkien', false);
+addNewBook('The Lord of the Rings', 'J.R.R Tolkien', false);
+
+addNewBook('The Lord of the Rings', 'J.R.R Tolkien', false);
+
+addNewBook('The Lord of the Rings', 'J.R.R Tolkien', false);
+
+addNewBook('The Lord of the Rings', 'J.R.R Tolkien', false);
+
+addNewBook('The Lord of the Rings', 'J.R.R Tolkien', false);
+
+
+
+
+
