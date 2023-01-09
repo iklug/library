3a106660629dc045d31e00a7f67c639320c40d10
Ian Klug <ianklug123@gmail.com>
2023-01-08 17:26:07 -0800

    done for now

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 let myLibrary = [];
 
 
-function Book(title, author, pages, haveRead) {
+function Book(title, author, haveRead) {
     this.title = title;
     this.author = author;
     this.haveRead = haveRead;
@@ -12,71 +12,76 @@ function Book(title, author, pages, haveRead) {
     }
 
 }
-const book1 = new Book('The Hobbit', 'J.R.R. Tolkien', 295, true);
-console.log(book1.title);
-console.log(book1.author);
-console.log(book1.info());
-
-const numbers = {
-    numberA: 5,
-    numberB: 10,
-    sum: function() {
-        console.log(this === numbers);
-        const calculate = () => {
-            console.log(this === numbers);
-            return this.numberA + this.numberB;
-        }
-        return calculate();
-    }
-}
-console.log(numbers.sum());
-
+const book1 = new Book('The Hobbit', 'J.R.R. Tolkien', true);
+const book2 = new Book('Catch-22', 'Joseph Heller', false);
 const addBookToLibrary = bookObject => {
 myLibrary.push(bookObject);
 console.log( `${bookObject.title} has been added to the Library.`);
 
 }
 addBookToLibrary(book1);
-console.log(myLibrary);
+addBookToLibrary(book2);
 
 // html related manipulation
 
 const bookList = document.querySelector('div.book-display');
 
-const addNewBook = (title, author, haveRead) => {
+const addNewBook = (obj) => {
     const bookList = document.querySelector('div.book-display');
     const newBook = document.createElement('div');
 newBook.classList.add('listed-book');
 newBook.setAttribute('data-index', `${myLibrary.length}`);
 const bookTitle = document.createElement('div');
 bookTitle.classList.add('book-title');
-bookTitle.textContent = title;
+bookTitle.textContent = obj.title;
 const bookAuthor = document.createElement('div');
-bookAuthor.textContent = author;
+bookAuthor.textContent = obj.author;
 const buttonDiv = document.createElement('div');
 const readButton = document.createElement('button');
 readButton.classList.add('listed');
-haveRead ? readButton.classList.add('read-border') : readButton.classList.add('unread-border');
+obj.haveRead ? console.log('something') : readButton.classList.add('unread-border');
 
-haveRead ? readButton.textContent = 'Read' : readButton.textContent = 'Not Read';
+obj.haveRead ? readButton.textContent = 'Read' : readButton.textContent = 'Not Read';
 buttonDiv.appendChild(readButton);
 newBook.appendChild(bookTitle);
 newBook.appendChild(bookAuthor);
 newBook.appendChild(buttonDiv);
 bookList.appendChild(newBook);
 }
-addNewBook('The Lord of the Rings', 'J.R.R Tolkien', false);
-addNewBook('The Lord of the Rings', 'J.R.R Tolkien', false);
 
-addNewBook('The Lord of the Rings', 'J.R.R Tolkien', false);
+const displayBooks = (array) => {
+    for(let index in array){
+        addNewBook(array[index]);
+    }
+}
+displayBooks(myLibrary);
 
-addNewBook('The Lord of the Rings', 'J.R.R Tolkien', false);
+// const changeBorder = (button) => {
+//     button.classList.remove('read-border');
+//     button.classList.add('unread-border');
+// }
 
-addNewBook('The Lord of the Rings', 'J.R.R Tolkien', false);
-
-addNewBook('The Lord of the Rings', 'J.R.R Tolkien', false);
-
-
-
+const buttons = document.querySelectorAll('.listed');
 
 
+
+
+Array.from(buttons).forEach(key => key.addEventListener('click', (key) => {
+    
+    key.target.classList.toggle('unread-border');
+    if(key.target.getAttribute('class') === 'listed unread-border'){
+        return key.target.textContent = 'Not Read';
+    } 
+    key.target.textContent = 'Read';
+
+
+}
+        
+    
+));
+// Array.from(badButtons).forEach(key => key.addEventListener('click', (key) =>
+//     {key.target.classList.remove('unread-border'); 
+//         key.target.classList.add('read-border');
+//         key.target.textContent = 'Read';
+//         }
+// ));
